42ce0bd33927675e35e4094c6f4391aa7fa2fe63
Jonathan Christopher <jonathan@mondaybynoon.com>
2014-08-11 08:39:30 -0400

    Adds duplicate_menu_item action. Formatting.

--- duplicate-menu.php
+++ duplicate-menu.php
@@ -5,11 +5,11 @@ Plugin Name: Duplicate Menu
 Plugin URI: https://github.com/jchristopher/duplicate-menu
 Description: Easily duplicate your WordPress Menus
 Author: Jonathan Christopher
-Version: 0.1.1
+Version: 0.2
 Author URI: http://mondaybynoon.com
 */
 
-/*  Copyright 2011 - 2012 Jonathan Christopher (email : jonathan@mondaybynoon.com)
+/*  Copyright 2011-2014 Jonathan Christopher (email : jonathan@mondaybynoon.com)
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -26,57 +26,43 @@ Author URI: http://mondaybynoon.com
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-
-// constant definitions
-if( !defined( 'IS_ADMIN' ) )
-    define( 'IS_ADMIN',  is_admin() );
-
-define( 'DUPLICATE_MENU_VERSION',   '0.1.1' );
+define( 'DUPLICATE_MENU_VERSION',   '0.2' );
 define( 'DUPLICATE_MENU_DIR',       plugin_dir_path( __FILE__ ) );
 define( 'DUPLICATE_MENU_URL',       plugin_dir_url( __FILE__ ) );
 
-function duplicate_menu_options_page()
-{
+function duplicate_menu_options_page() {
     add_theme_page( 'Duplicate Menu', 'Duplicate Menu', 'manage_options', 'duplicate-menu', array( 'DuplicateMenu', 'options_screen' ) );
 }
 
-// WordPress actions
-if( IS_ADMIN )
-{
-    add_action( 'admin_menu', 'duplicate_menu_options_page' );
-
-}
+add_action( 'admin_menu', 'duplicate_menu_options_page' );
 
 /**
 * Duplicate Menu
 */
-class DuplicateMenu
-{
+class DuplicateMenu {
 
-    function duplicate( $id = null, $name = null )
-    {
+    function duplicate( $id = null, $name = null ) {
+
         // sanity check
-        if( empty( $id ) || empty( $name ) )
-            return false;
+        if ( empty( $id ) || empty( $name ) ) {
+	        return false;
+        }
 
-        $id             = intval( $id );
-        $name           = sanitize_text_field( $name );
-
-        $source         = wp_get_nav_menu_object( $id );
-        $source_items   = wp_get_nav_menu_items( $id );
-
+        $id = intval( $id );
+        $name = sanitize_text_field( $name );
+        $source = wp_get_nav_menu_object( $id );
+        $source_items = wp_get_nav_menu_items( $id );
         $new_id = wp_create_nav_menu( $name );
 
-        if( !$new_id )
+        if ( ! $new_id ) {
             return false;
+        }
 
         // key is the original db ID, val is the new
         $rel = array();
 
         $i = 1;
-        foreach( $source_items as $menu_item )
-        {
-
+        foreach ( $source_items as $menu_item ) {
             $args = array(
                 'menu-item-db-id'       => $menu_item->db_id,
                 'menu-item-object-id'   => $menu_item->object_id,
@@ -98,11 +84,13 @@ class DuplicateMenu
             $rel[$menu_item->db_id] = $parent_id;
 
             // did it have a parent? if so, we need to update with the NEW ID
-            if( $menu_item->menu_item_parent )
-            {
+            if ( $menu_item->menu_item_parent ) {
                 $args['menu-item-parent-id'] = $rel[$menu_item->menu_item_parent];
                 $parent_id = wp_update_nav_menu_item( $new_id, $parent_id, $args );
             }
+
+	        // allow developers to run any custom functionality they'd like
+	        do_action( 'duplicate_menu_item', $menu_item, $args );
 
             $i++;
         }
@@ -110,15 +98,14 @@ class DuplicateMenu
         return $new_id;
     }
 
-    function options_screen()
-    {
+    function options_screen() {
         $nav_menus = wp_get_nav_menus();
     ?>
     <div class="wrap">
         <div id="icon-options-general" class="icon32"><br /></div>
             <h2><?php _e( 'Duplicate Menu' ); ?></h2>
 
-            <?php if( !empty( $_POST ) && wp_verify_nonce( $_POST['duplicate_menu_nonce'], 'duplicate_menu' ) ) : ?>
+            <?php if ( ! empty( $_POST ) && wp_verify_nonce( $_POST['duplicate_menu_nonce'], 'duplicate_menu' ) ) : ?>
                 <?php
                     $source         = intval( $_POST['source'] );
                     $destination    = sanitize_text_field( $_POST['new_menu_name'] );
@@ -129,8 +116,8 @@ class DuplicateMenu
                 ?>
 
                 <div id="message" class="updated"><p>
-                    <?php if( $new_menu_id ) : ?>
-                        <?php _e( 'Menu Duplicated' ) ?>. <a href="nav-menus.php?action=edit&amp;menu=<?php echo $new_menu_id; ?>"><?php _e( 'View' ) ?></a>
+                    <?php if ( $new_menu_id ) : ?>
+                        <?php _e( 'Menu Duplicated' ) ?>. <a href="nav-menus.php?action=edit&amp;menu=<?php echo absint( $new_menu_id ); ?>"><?php _e( 'View' ) ?></a>
                     <?php else: ?>
                         <?php _e( 'There was a problem duplicating your menu. No action was taken.' ) ?>.
                     <?php endif; ?>
@@ -139,7 +126,7 @@ class DuplicateMenu
             <?php endif; ?>
 
 
-            <?php if( empty( $nav_menus ) ) : ?>
+            <?php if ( empty( $nav_menus ) ) : ?>
                 <p><?php _e( "You haven't created any Menus yet." ); ?></p>
             <?php else: ?>
                 <form method="post" action="">
@@ -151,7 +138,7 @@ class DuplicateMenu
                             </th>
                             <td>
                                 <select name="source">
-                                    <?php foreach( (array) $nav_menus as $_nav_menu ) : ?>
+                                    <?php foreach ( (array) $nav_menus as $_nav_menu ) : ?>
                                         <option value="<?php echo esc_attr($_nav_menu->term_id) ?>">
                                             <?php echo esc_html( $_nav_menu->name ); ?>
                                         </option>
